51f2c89a0a6c2c417c880daa1520f2ad9c60ca50
metajyotiagrawal <jyoti.agrawal@metacube.com>
2016-08-08 18:22:24 +0530

    Add files via upload

---
+++ Admin.java
@@ -0,0 +1,80 @@
+package DS4;
+
+import java.util.Scanner;
+/**
+ * class to to handle the jobs sent to the admin
+ * @author jyoti
+ *
+ */
+public class Admin {
+	public Job[] jobHeap = new Job[100];
+	int largest = 0;
+	int heapSize = 0;
+
+	public void insertnewJob(Scanner sc) {
+        System.out.println("Enter job profile");
+		String str = sc.next();
+		Job newjob = new Job(str);
+		Insert(newjob);
+
+	}
+/*
+ * to insert jb object in array jobheap
+ * jb object containing job profile and priority
+ */
+	public void Insert(Job jb) {
+		jobHeap[heapSize] = jb;
+		for (int i = heapSize / 2; i >= 0; i--) {
+			heapify(i);
+
+		}
+		heapSize++;
+	}
+/*
+ * to heapify the heap array jobHeap
+ */
+	public void heapify(int i) {
+
+		int L = 2 * i + 1;
+		int R = 2 * i + 2;
+		if (L <= heapSize && jobHeap[L].priority > jobHeap[i].priority) {
+			largest = L;
+
+		} else {
+			largest = i;
+		}
+		if (R <= heapSize && jobHeap[R].priority > jobHeap[largest].priority) {
+			largest = R;
+		}
+		if (largest != i) {
+			Job swap = new Job();
+			swap = jobHeap[i];
+			jobHeap[i] = jobHeap[largest];
+			jobHeap[largest] = swap;
+		}
+	}
+/*
+ * to display job on the basis of priority
+ */
+	public void displayJob() {
+		for (int cnt = 0; cnt < heapSize; cnt++) {
+			System.out.println(jobHeap[cnt].profile);
+		}
+
+	}
+
+	public static void main(String args[]) {
+		Admin ad = new Admin();
+        Scanner sc = new Scanner(System.in);
+		int exit=1;
+		while (exit==1) {
+			ad.insertnewJob(sc);
+			System.out.println("want to continue enter number other then 1");
+			exit = sc.nextInt();
+
+		}
+		ad.displayJob();
+		sc.close();
+	}
+
+}
